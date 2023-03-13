8498b8692162c20573672264a566a833bf175a77
AVELOPEZ <2190102@usap.edu>
2023-03-12 19:43:53 -0600

    cam4

--- index.php
+++ index.php
@@ -54,8 +54,8 @@
       </form>
       
       </ul>
-      <span class="material-symbols-outlined" class="icon"> person </span>
-      <i class="bi bi-cart-plus-fill"></i>
+      <span class="material-symbols-outlined" class="icon" style="font-size: 50px"> person </span>
+      <i class="bi bi-cart-plus-fill" style="font-size: 50px"></i>
     </div>
   </div>
 </nav>
@@ -143,54 +143,139 @@
     </div>
 </div>
   <!--lo mas vendido-->
- <section>
+ <section  >
+    <div class="card-deck">
    <div class="container" style="margin-bottom: 1%">
      <div class="borde">
-        <H2 class="centro"> NUESTROS SERVICIOS MAS SOLICITADOS</H2>
-        <div class="col-12 col-md-12 col-lg-9">
-            <div class="row">
-              <div
-                class="card posimagenes col-6 col-sm-6 col-md-3 col-lg-3"
-                style="width: 14rem; height: 20rem"
-              >
-                <img src="img/jar2.jpg" class="card-img-top" alt="" />
-                <div class="card-body">
-                  <h5 class="card-title">Titulo 1</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
+        <H2 class="centro"> NUESTRAS CATEGORIAS</H2>
+        <div class="col-12 col-md-12 col-lg-9  " style="width: 100%">
+        <div class="row justify-content-center ">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="img/jar2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a short card.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>        
+<!------ Tarjetas ----->
+<div class="col-12 col-md-12 col-lg-9  " style="width: 100%">
+<div class="row justify-content-center ">
+<section class="personal row justify-content-center">
+  <div class="row mt-3">
+    <div class="card-group g-2">
+      <!--- Tarjeta 1 --->
+      <div class="col">
+        <div class="card text-bg-dark" style="width: 18rem;">
+          <img src="https://st1.uvnimg.com/dims4/default/c0b3972/2147483647/thumbnail/1240x698/quality/75/?url=https%3A%2F%2Fuvn-brightspot.s3.amazonaws.com%2Fassets%2Fvixes%2Fimj%2Fvivirsalud%2FL%2FLas-caracteristicas-de-los-diferentes-tipos-de-rostros-2.jpg" width="50" height="140" class="card-img-top img-border">
+          <div class="card-body text-light">
+            <h5 class="card-title">Adriana Carranzas</h5>
+            <div class="iconos">
+              <ul class="action">
+                <div class="ranking">
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts fa-gray"></i> (6)
                 </div>
-              </div>
-              <div
-                class="card posimagenes col-6 col-sm-6 col-md-3 col-lg-3"
-                style="width: 14rem; height: 25rem"
-              >
-                <img src="img/refri.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">Titulo 1</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-              </div>
-             
-              <div
-                class="card posimagenes col-6 col-sm-6 col-md-3 col-lg-3"
-                style="width: 14rem; height: 25rem"
-              >
-                <img src="img/elec.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">Titulo 1</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-              </div>
+                <i class="bi bi-clock"> 5 años de experiencia</i>
+                <li><i class="bi bi-coin"> L 900.00</i></li>
+                <li><i class="bi bi-telephone-fill"> 8754-2555</i></li>
+              </ul>
             </div>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="card-footer text-center">
+              <a href="#" class="btn">Reservar</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--- Tarjeta 2 --->
+      <div class="col">
+        <div class="card text-bg-dark" style="width: 18rem;">
+          <img src="img/per1.jpg" width="50" height="140" class="card-img-top img-border">
+          <div class="card-body text-light">
+            <h5 class="card-title">Card title</h5>
+            <div class="iconos">
+              <ul class="action">
+                <div class="ranking">
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts fa-gray"></i> (6)
+                </div>
+                <i class="bi bi-clock"> 5 años de experiencia</i>
+                <li><i class="bi bi-coin"> L 900.00</i></li>
+                <li><i class="bi bi-telephone-fill"> 8754-2555</i></li>
+              </ul>
+            </div>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="card-footer text-center">
+              <a href="#" class="btn">Reservar</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--- Tarjeta 3 --->
+      <div class="col">
+        <div class="card text-bg-dark" style="width: 18rem;">
+          <img class="card-img-top img-border" src="http://resizer.sevilla.abc.es/resizer/resizer.php?imagen=https://sevilla.abc.es/estilo/bulevarsur/wp-content/uploads/sites/14/2020/07/cortes-de-pelo-adelgazan-rostro.jpg&nuevoancho=652" width="50" height="140">
+          <div class="card-body text-light">
+            <h5 class="card-title">Card title</h5>
+            <div class="iconos">
+              <ul class="action">
+                <div class="ranking">
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts"></i>
+                  <i class="bi bi-star-fill starts fa-gray"></i> (6)
+                </div>
+                <i class="bi bi-clock"> 5 años de experiencia</i>
+                <li><i class="bi bi-coin"> L 900.00</i></li>
+                <li><i class="bi bi-telephone-fill"> 8754-2555</i></li>
+              </ul>
+            </div>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="card-footer text-center">
+              <a href="#" class="btn">Reservar</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+    </div>
+  </div>
+  </section>       
+</div>
+</div>
 </section>
+
+
+
 </div>
 </div>
 </div>
