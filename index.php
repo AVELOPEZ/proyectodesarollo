aa01e9c37851ce05437bb73bdd2b457e726847df
AVELOPEZ <2190102@usap.edu>
2023-03-12 21:33:36 -0600

    c5

--- index.php
+++ index.php
@@ -186,9 +186,9 @@
     <div class="card-group g-2">
       <!--- Tarjeta 1 --->
       <div class="col">
-        <div class="card text-bg-dark" style="width: 18rem;">
+        <div class="card text-bg-light" style="width: 18rem;">
           <img src="https://st1.uvnimg.com/dims4/default/c0b3972/2147483647/thumbnail/1240x698/quality/75/?url=https%3A%2F%2Fuvn-brightspot.s3.amazonaws.com%2Fassets%2Fvixes%2Fimj%2Fvivirsalud%2FL%2FLas-caracteristicas-de-los-diferentes-tipos-de-rostros-2.jpg" width="50" height="140" class="card-img-top img-border">
-          <div class="card-body text-light">
+          <div class="card-body text-dark">
             <h5 class="card-title">Adriana Carranzas</h5>
             <div class="iconos">
               <ul class="action">
@@ -213,9 +213,9 @@
       </div>
       <!--- Tarjeta 2 --->
       <div class="col">
-        <div class="card text-bg-dark" style="width: 18rem;">
+        <div class="card text-bg-light" style="width: 18rem;">
           <img src="img/per1.jpg" width="50" height="140" class="card-img-top img-border">
-          <div class="card-body text-light">
+          <div class="card-body  text-dark">
             <h5 class="card-title">Card title</h5>
             <div class="iconos">
               <ul class="action">
@@ -240,9 +240,9 @@
       </div>
       <!--- Tarjeta 3 --->
       <div class="col">
-        <div class="card text-bg-dark" style="width: 18rem;">
+        <div class="card text-bg-light" style="width: 18rem;">
           <img class="card-img-top img-border" src="http://resizer.sevilla.abc.es/resizer/resizer.php?imagen=https://sevilla.abc.es/estilo/bulevarsur/wp-content/uploads/sites/14/2020/07/cortes-de-pelo-adelgazan-rostro.jpg&nuevoancho=652" width="50" height="140">
-          <div class="card-body text-light">
+          <div class="card-body  text-dark">
             <h5 class="card-title">Card title</h5>
             <div class="iconos">
               <ul class="action">
